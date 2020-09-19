5be0cc744e7c2877c2778a7b983a10e07cf3972c
Joel Grus <joelgrus@gmail.com>
2020-09-18 17:22:43 -0700

    add description

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dumb-language-model
 just a dumb language model in a streamlit app
+
+```python
+# use a virtual environment or whatever
+$ pip install -r requirements.txt
+$ streamlit run app.py
+```
--- app.py
+++ app.py
@@ -7,6 +7,24 @@ from spacy_streamlit import process_text
 
 st.title("Dumb Language Model")
 
+st.markdown("""
+Back before GPT-3 and all that we used to make our own language models
+by hand, uphill both ways, etc etc. This right here is a dead-simple
+[n-gram language model](https://en.wikipedia.org/wiki/N-gram#n-gram_models).
+
+The pre-filled corpus is all of Shakespeare's sonnets, 
+which means it will generate nonsense that looks like poetry.
+Of course, one man's "nonsense that looks like poetry" is another man's poetry.
+
+If you want to put in your own text, know that the code assumes that 
+blank lines are paragraph dividers, and that the generation code stops
+when it generates such a divider. This seemed like a good way to generate
+a "poem worth" of material.
+
+If you want to see the small amount of code behind this, it's
+[on GitHub](https://github.com/joelgrus/dumb-language-model).
+""")
+
 with open('sonnets.txt') as f:
     TEXT = f.read()
 
